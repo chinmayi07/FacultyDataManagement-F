cd04f373abada9d408eb97c3cb6844e7b22aae23
mb-charlie <92309564+mb-charlie@users.noreply.github.com>
2022-05-08 20:33:35 +0530

    sending pdf or excel file to backend

--- src/Screens/Landing.js
+++ src/Screens/Landing.js
@@ -3,12 +3,12 @@ import '../Styles/TopNavbar.css'
 import TopNavbar from '../Components/TopNavbar';
 
 export default function Landing(props) {
-    const [image, setImage] = useState({ preview: '', data: '' })
+    const [fileData, setFileData] = useState({ preview: '', data: '' })
     const [status, setStatus] = useState('')
     const handleSubmit = async (e) => {
         e.preventDefault()
         let formData = new FormData()
-        formData.append('file', image.data)
+        formData.append('file', fileData.data)
         const response = await fetch('http://localhost:3001/upload', {
             method: 'POST',
             body: formData,
@@ -16,18 +16,17 @@ export default function Landing(props) {
         if (response) setStatus(response.statusText)
     }
     const handleFileChange = (e) => {
-        const img = {
+        const fileInfo = {
             preview: URL.createObjectURL(e.target.files[0]),
             data: e.target.files[0],
         }
-        setImage(img)
+        setFileData(fileInfo)
     }
     return (
         <>
             <TopNavbar />
             <div className='App'>
                 <h1>Upload to server</h1>
-                {image.preview && <img src={image.preview} width='100' height='100' alt='Upload to server' />}
                 <hr></hr>
                 <form onSubmit={handleSubmit}>
                     <input type='file' name='file' onChange={handleFileChange}></input>
